25276100e2c6479bc793a7740880b3acf183e997
Ken Murrell <ken.murrell13@gmail.com>
2017-11-29 15:37:41 -0500

    Preliminary xhtml, locale and authentication designs 2017-11-29

---
+++ src/java/filters/Auth.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author kenmu
+ */
+@WebFilter(filterName = "Auth", urlPatterns = {"/*"})
+public class Auth implements Filter {
+
+    private FilterConfig filterConfig = null;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession ses = req.getSession(false);  
+        String reqURI = req.getRequestURI();
+       
+        //CHANGE THIS TO USERS
+        if (reqURI.contains("/protected/") && (ses == null || (ses != null && ses.getAttribute("User") == null))) {
+            String loginURL = req.getContextPath() + "/faces/login.xhtml";
+            res.sendRedirect(loginURL);
+        } else {
+            chain.doFilter(request, response);
+        } 
+        //CHANGE THIS TO USERS
+            
+    }
+
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+    @Override
+    public void destroy() {        
+    }
+    @Override
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+    }
+
+}
